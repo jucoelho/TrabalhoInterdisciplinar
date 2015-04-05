8d635ca9ac52bda6022b55ee37c60162a3b82456
dfventura <danilo@anaton.com.br>
2015-04-05 13:09:54 -0300

    Criação o select
    craido setect e tabela de exemplo

--- produto/codigo/relatorio.php
+++ produto/codigo/relatorio.php
@@ -17,16 +17,41 @@
 
 	<body>
 		<div id="container">
-			<?php include 'includes/top-bar.php';?>
-			<div id="header" class="header"><?php include 'includes/header.php';?></div>
-			<?php include 'includes/menubar.php';?>
-			<div id="cabecalho"><?php include 'includes/cabecalho.php';?></div>
-			<div id="content" class="content">
-				<!--<h1><a id="link" href=""></h1>-->
-		
-			</div>
-			<br style="clear:both"/>
-			<div id="footer" class="footer"><?php include 'includes/footer.php'; ?></div>
+                    <?php include 'includes/top-bar.php';?>
+                    <div id="header" class="header"><?php include 'includes/header.php';?></div>
+                    <?php include 'includes/menubar.php';?>
+                    <div id="cabecalho"><?php include 'includes/cabecalho.php';?></div>
+                    <div id="content" class="content">
+                        <!--<h1><a id="link" href=""></h1>-->
+                        <select name="Filtrar">
+                            <option value="categoria">Categoria</option>
+                            <option value="quantidade">Quantidade</option>                                      
+                        </select>
+                    <div id="tabelaCadastro">
+                        <table >
+                            <tr>
+                                    <th> Código</th>
+                                    <th> Descrição</th>
+                                    <th> Preço</th>
+                                    <th> Quantidade</th>
+                                    <th > Editar</th>
+                                    <th> Excluir</th>
+                            </tr>
+                            <tbody>
+                              <tr>
+                                    <td> teste</td>
+                                    <td> teste</td>
+                                    <td> teste</td>
+                                    <td> teste</td>
+                                    <td onclick="editar()" id="colunaEditar"> Editar</td>
+                                    <td id="colunaExcluir"> Excluir</td>
+                              </tr>
+                            </tbody>
+                        </table>	
+                    </div>       
+                    </div>
+                    <br style="clear:both"/>
+                    <div id="footer" class="footer"><?php include 'includes/footer.php'; ?></div>
 		</div>
 	</body>
 </html>
